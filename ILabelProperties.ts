e32d2c4b30c8307ab3819dea4c3c304535e68dbc
Mukundloya <97350065+Mukundloya@users.noreply.github.com>
2022-01-30 02:46:42 +0530

    Delete ILabelProperties.ts

--- ILabelProperties.ts
+++
@@ -1,8 +0,0 @@
-export interface ILabelProperties {
-  label: string | undefined;
-  fontSize: string | undefined;
-  fontWeight: string | undefined;
-  clientX: number | undefined;
-  clientY: number | undefined;
-  element: string;
-}

3bee0a6d3fed4c9eb1472549f2e3d383cf808fbf
Mukundloya <97350065+Mukundloya@users.noreply.github.com>
2022-01-30 02:46:37 +0530

    Delete IElementProperties.ts

--- IElementProperties.ts
+++
@@ -1,7 +0,0 @@
-import { CSSProperties } from "react";
-import { ILabelProperties } from "./ILabelProperties";
-
-export interface IElementProperties {
-  labelProperties: ILabelProperties | undefined;
-  labelStyle: CSSProperties | undefined;
-}
